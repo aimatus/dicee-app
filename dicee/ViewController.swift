62079b365ff8ea6d39c33c0851ce024562f31e3c
Abraham Matus <aimatusco@gmail.com>
2018-11-27 11:30:49 -0500

    Generates random dice numbers on app start up

--- dicee/ViewController.swift
+++ dicee/ViewController.swift
@@ -11,16 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        rollDices()
     }
 
     @IBAction func onRollButtonPressed(_ sender: UIButton) {
+        rollDices()
+    }
+
+    func rollDices() {
         leftDiceImageView.image = getRandomDiceImage()
         rightDiceImageView.image = getRandomDiceImage()
     }
 
     func getRandomDiceImage() -> UIImage? {
         let randomDiceNumber: Int = Int.random(in: START_DOMINO_NUMBER ... LAST_DOMINO_NUMBER)
-        let diceImageName: String = DICE_IMAGE_NAME_PREFIX + String(randomDiceNumber)
+        let diceImageName: String = "\(DICE_IMAGE_NAME_PREFIX)\(randomDiceNumber)"
         return UIImage(named: diceImageName)
     }
 }
